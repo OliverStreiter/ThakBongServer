d93eb105b8930712bfef4e2aa6e716b243586a37
Sunny <sunnybaby28@gmail.com>
2023-11-08 13:59:18 +0800

    update database

--- db.js
+++ db.js
@@ -2,20 +2,20 @@ const Pool = require("pg").Pool
 
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-// const pool = new Pool({
-//     connectionString: process.env.POSTGRES_URL
-// })
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL
+})
 
 
 
 // local
-const pool = new Pool({
-    user: "postgres",
-    password: "Sunny0828",
-    database: "thakbong_database",
-    host: "localhost",
-    port: 5432
-})
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "Sunny0828",
+//     database: "thakbong_database",
+//     host: "localhost",
+//     port: 5432
+// })
 
 pool.connect((err) => {
     if (err) throw err
